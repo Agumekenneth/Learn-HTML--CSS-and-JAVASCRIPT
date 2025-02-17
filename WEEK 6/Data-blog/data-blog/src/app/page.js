80d82d8768b6871203d5c4c097aeeb2cf2f18a00
Agume_kenneth <180535228+Agumekenneth@users.noreply.github.com>
2025-02-17 13:00:53 +0300

    changes

--- WEEK 6/Data-blog/data-blog/src/app/page.js
+++ WEEK 6/Data-blog/data-blog/src/app/page.js
@@ -1,8 +1,16 @@
 export default function Home(){
+  let firstNumber = 938
+  let secondNumner = 7
+  let sum = firstNumber + secondNumner
 
-return <div>
+return (
+<div>
   <h1>DATA SCIENCE FOR AFRICA </h1>
   <h2>An intiative for 2025</h2>
   <p>Welcome to our website</p>
+  <strong>{10+10*1000}</strong> 
+  <br/>
+  <em>{sum}</em>
 </div>
+)
 }
